df34e02439ba18c915ab74493722bfad96555586
themanyfaceddemon <neko-d-05@mail.ru>
2025-12-31 16:56:16 +0300

    fix some bugs

--- app.py
+++ app.py
@@ -5,23 +5,17 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from router.overlord_api import router as overlord_router
-from router.proxy import router as proxy_router
-from router.static_accel import router as static_accel_router
-from services import poll_services
+from router.game import router as game_api_router
+from router.overlord_api import router as overlord_api_router
 
 
 @contextlib.asynccontextmanager
 async def lifespan(app: FastAPI):
-    task = asyncio.create_task(poll_services())
-
     try:
         yield
 
     finally:
-        task.cancel()
-        with contextlib.suppress(asyncio.CancelledError):
-            await task
+        pass
 
 
 app = FastAPI(
@@ -38,6 +32,5 @@ if os.getenv("FASTAPISTATIC") == "1":
         name="static",
     )
 
-app.include_router(overlord_router)
-app.include_router(static_accel_router)
-app.include_router(proxy_router)
+app.include_router(overlord_api_router)
+app.include_router(game_api_router)
